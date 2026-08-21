6a36200e408dc5ea4291bf8da1f1a2218146e333
Jasurbek09 <omonqulovjasurbek04@gmail.com>
2026-08-21 04:04:15 -0700

    mian

--- src/components/3d/index.ts
+++
@@ -1,8 +0,0 @@
-export { Hero3D } from "@/components/3d/hero-3d";
-export { InteractiveProduct } from "@/components/3d/interactive-product";
-export { MilkDropAnimation } from "@/components/3d/milk-drop-animation";
-export { ParallaxNature } from "@/components/3d/parallax-nature";
-export { ParticleField } from "@/components/3d/particle-field";
-export { Product3DViewer } from "@/components/3d/product-3d-viewer";
-export { SceneLoader } from "@/components/3d/scene-loader";
-export { WebGLFallback } from "@/components/3d/webgl-fallback";
